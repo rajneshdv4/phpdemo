f1d37eb40645d1a381eae43fad71ce246ed22bfe
rajneshdv4 <rajneshdv4@gmail.com>
2019-09-03 10:44:00 +0530

    Add Html code

---
+++ raj.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>Online PHP Script Execution</title>
+</head>
+<body>
+<?php
+for($number =1; $number<100;$number ++){
+
+if ($number%5==0) {
+   echo $number.'<br>';}}
+?>
+</body>
+</html>
